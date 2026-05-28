753b2d3b7d6d1fb68147491fe2309fcf3f935613
Arjun Vijay Anup <88475666+arjunvijayanup@users.noreply.github.com>
2026-05-28 13:38:51 +0100

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count, chmap = {}, {}
+        l = 0
+        for c in s1:
+            chmap[c] = 1 + chmap.get(c, 0)
+    
+        for r in range(len(s2)):
+            count[s2[r]] = 1 + count.get(s2[r], 0)
+            if (r - l + 1) > len(s1):
+                count[s2[l]] -= 1
+                if count[s2[l]] == 0:
+                    del count[s2[l]]
+                l += 1
+            if count == chmap:
+                return True
+        
+        return False
+            
+
+
+
